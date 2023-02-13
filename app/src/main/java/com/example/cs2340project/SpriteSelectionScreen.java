4a0ddf28a09a56df177f14e64acabd61c639c8b7
r4c3 <63271957+r4c3@users.noreply.github.com>
2023-02-13 14:46:08 -0500

    added confirmation on sprite selection

--- app/src/main/java/com/example/cs2340project/SpriteSelectionScreen.java
+++ app/src/main/java/com/example/cs2340project/SpriteSelectionScreen.java
@@ -1,6 +1,7 @@
 package com.example.cs2340project;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageButton;
@@ -9,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SpriteSelectionScreen extends AppCompatActivity {
-
+    private ImageButton spriteSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +21,38 @@ public class SpriteSelectionScreen extends AppCompatActivity {
         ImageButton blueFrogBtn = (ImageButton) findViewById(R.id.blue_frog);
         ImageButton redFrogBtn = (ImageButton) findViewById(R.id.red_frog);
 
-        greenFrogBtn.setOnClickListener(view -> onBtnClick(R.drawable.green_frog));
-        blueFrogBtn.setOnClickListener(view -> onBtnClick(R.drawable.blue_frog));
-        redFrogBtn.setOnClickListener(view -> onBtnClick(R.drawable.red_frog));
+        greenFrogBtn.setOnClickListener(view -> onBtnClick(R.drawable.green_frog, greenFrogBtn));
+        blueFrogBtn.setOnClickListener(view -> onBtnClick(R.drawable.blue_frog, blueFrogBtn));
+        redFrogBtn.setOnClickListener(view -> onBtnClick(R.drawable.red_frog, redFrogBtn));
     }
 
 
-    public void onBtnClick(int color) {
-        Intent intent = new Intent(SpriteSelectionScreen.this, GameScreen.class);
+    public void onBtnClick(int color, ImageButton frogClicked) {
+        if (frogClicked.equals(spriteSelected)) {
+            Intent intent = new Intent(SpriteSelectionScreen.this, GameScreen.class);
+            Bundle bundle = getIntent().getExtras();
 
-        Bundle bundle = getIntent().getExtras();
+            //specific sprite is an int because R.drawable.x is an id
+            bundle.putInt("spriteColor", color);
+            System.out.println(color);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        } else {
+            spriteSelected = frogClicked;
 
-        //specific sprite is an int because R.drawable.x is an id
-        bundle.putInt("spriteColor", color);
-        System.out.println(color);
-        intent.putExtras(bundle);
-        startActivity(intent);
+            //i tried pulling this out to class scope but it caused errors
+            ImageButton greenFrogBtn = (ImageButton) findViewById(R.id.green_frog);
+            ImageButton blueFrogBtn = (ImageButton) findViewById(R.id.blue_frog);
+            ImageButton redFrogBtn = (ImageButton) findViewById(R.id.red_frog);
+
+            //set all frog backgrounds back to blue
+            greenFrogBtn.setBackgroundColor(Color.parseColor("#52b9ff"));
+            blueFrogBtn.setBackgroundColor(Color.parseColor("#52b9ff"));
+            redFrogBtn.setBackgroundColor(Color.parseColor("#52b9ff"));
+
+            //set the clicked one to yellow
+            frogClicked.setBackgroundColor(Color.parseColor("#ebd160"));
+        }
     }
 
 }
