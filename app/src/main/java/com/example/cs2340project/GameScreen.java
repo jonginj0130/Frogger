7cccaad94cf67255edd99b520d33571127a20dec
r4c3 <63271957+r4c3@users.noreply.github.com>
2023-02-24 15:38:36 -0500

    Revert "Revert "Merge branch 'main' of https://github.gatech.edu/Team31/Sprint-1""
    This reverts commit 2080130ce5f38749e2cf22c539d2966a56b2ba6a.

--- app/src/main/java/com/example/cs2340project/GameScreen.java
+++ app/src/main/java/com/example/cs2340project/GameScreen.java
@@ -10,6 +10,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class GameScreen extends AppCompatActivity {
+    int numHearts = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +18,6 @@ public class GameScreen extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         String diff = bundle.getString("diff");
-        int numHearts = 3;
         if (diff.equals("Easy")) {
             numHearts = 5;
         } else if (diff.equals("Medium")) {
@@ -26,14 +26,17 @@ public class GameScreen extends AppCompatActivity {
         drawLives(numHearts);
 
         ProgressBar pb = findViewById(R.id.timeBar);
-        CountDownTimer timer = new CountDownTimer(5000, 1000) {
+        CountDownTimer timer = new CountDownTimer(5000, 100) {
             @Override
             public void onTick(long millisUntilFinished) {
-                pb.setProgress((int) (millisUntilFinished / 1000));
+                pb.setProgress(Math.round(millisUntilFinished));
             }
 
             @Override
             public void onFinish() {
+                numHearts--;
+                drawLives(numHearts);
+                pb.setProgress(0);
                 Toast.makeText(GameScreen.this, "Time Over!", Toast.LENGTH_SHORT).show();
             }
         }.start();
@@ -46,25 +49,24 @@ public class GameScreen extends AppCompatActivity {
         // draw river tiles onto screen
         int resID;
         TileRow tileRow;
-        int tileSize = (int) Math.round((0.30 * screenHeight) / 4);
         for (int i = 1; i < 5; i++) {
             resID = getResources().getIdentifier("river" + i, "id", getPackageName());
             ImageView riverArea = findViewById(resID);
-            tileRow = new TileRow(this, tileSize, R.drawable.river, screenWidth, false);
+            tileRow = new TileRow(this, R.drawable.river,screenHeight, screenWidth, false);
             riverArea.setImageBitmap(tileRow.getTileRow());
         }
         // draw road tiles onto screen
-        tileSize = (int) Math.round((0.35 * screenHeight) / 5);
+//        tileSize = (int) Math.round((0.35 * screenHeight) / 5);
         for (int i = 1; i < 6; i++) {
             resID = getResources().getIdentifier("road" + i, "id", getPackageName());
             ImageView roadArea = findViewById(resID);
-            tileRow = new TileRow(this, tileSize, R.drawable.road, screenWidth, true);
+            tileRow = new TileRow(this, R.drawable.road, screenHeight, screenWidth, true);
             roadArea.setImageBitmap(tileRow.getTileRow());
         }
         // draw start tile onto screen
-        tileSize = (int) Math.round((0.0833 * screenHeight));
+//        tileSize = (int) Math.round((0.0833 * screenHeight));
         ImageView safeArea = findViewById(R.id.start);
-        tileRow = new TileRow(this, tileSize, R.drawable.grass, screenWidth, true);
+        tileRow = new TileRow(this, R.drawable.grass, screenHeight, screenWidth, true);
         safeArea.setImageBitmap(tileRow.getTileRow());
 
         //draw rest tile onto screen
@@ -72,14 +74,20 @@ public class GameScreen extends AppCompatActivity {
         safeArea.setImageBitmap(tileRow.getTileRow());
 
         //draw goal tile onto screen
-        safeArea = findViewById(R.id.goal);
-        tileRow = new TileRow(this, tileSize, R.drawable.gold, screenWidth, true);
-        safeArea.setImageBitmap(tileRow.getTileRow());
+        ImageView goalArea = findViewById(R.id.goal);
+        tileRow = new TileRow(this, R.drawable.gold, screenHeight, screenWidth, true);
+        goalArea.setImageBitmap(tileRow.getTileRow());
     }
 
+    // method to draw the hearts at the top left of the screen
     public void drawLives(int lives) {
         ImageView img;
         int resID;
+        for (int i = 1; i <= 5; i++) {
+            resID = getResources().getIdentifier("heart" + i, "id", getPackageName());
+            img = findViewById(resID);
+            img.setImageResource(0);
+        }
         for (int i = 1; i <= lives; i++) {
             resID = getResources().getIdentifier("heart" + i, "id", getPackageName());
             img = findViewById(resID);
